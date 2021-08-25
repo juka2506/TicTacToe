e0452dd3d3c18dc9b19d32ea5edb478b98e8c5fd
Julija Katukova <julija.katukova@gmail.com>
2021-08-25 21:32:06 +0300

    updated

--- src/com/company/Move.java
+++ src/com/company/Move.java
@@ -16,4 +16,20 @@ public class Move {
     public int getY() {
         return this.y;
     }
+
+    @Override
+    public String toString() {
+        return "Move{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
